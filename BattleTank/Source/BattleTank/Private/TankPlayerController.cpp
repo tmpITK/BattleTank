0df8678d36f3f5e0bf6d158fdf1497584b504113
tmpITK <torok.mark.patrik@gmail.com>
2019-02-16 10:51:45 +0100

    Overridden AController's BeginPlay method to log out what the PlayerController possesses.

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -5,5 +5,17 @@
 
 
 ATank* ATankPlayerController::GetControlledTank() const {
-	return Cast<ATank>(GetPawn());
+	return Cast<ATank>(GetPawn());;
+}
+
+void ATankPlayerController::BeginPlay(){
+	Super::BeginPlay();
+	UE_LOG(LogTemp, Warning, TEXT("Player controller begin play."));
+	auto ControlledTank = GetControlledTank();
+	if (ControlledTank) {
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController possessing %s."), *ControlledTank->GetName());
+	}
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController does not possess anything."));
+	}
 }
